17538548d61cb2b722bfbf0ea12b43fa8c809c14
Đô Đạt Đức <12345ddduc@gmail.com>
2022-05-22 02:17:23 +0700

    finished logout admin

--- src/features/Admin/components/LogOut/LogOut.jsx
+++ src/features/Admin/components/LogOut/LogOut.jsx
@@ -1,21 +1,47 @@
-import React from "react";
-import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import Cookies from "js-cookie";
+import React from "react";
+import { toast } from "react-toastify";
+import userAPI from "../../../../api/userAPI";
+import { useHistory } from "react-router-dom";
 
+toast.configure();
 LogOut.propTypes = {};
 
 function LogOut(props) {
+  const History = useHistory();
   const handleClose = () => {
     props.closeFormLogOut(false);
   };
 
-  const handleClickLogOut = () => {};
+  const handleClickLogOut = () => {
+    (async () => {
+      try {
+        const response = await userAPI.logout({
+          refreshToken: Cookies.get("refreshToken"),
+        });
+        if (response.status === 200) {
+          props.closeFormLogOut(false);
+          Cookies.remove("refreshToken");
+          Cookies.remove("token");
+          localStorage.removeItem("account");
+          History.push("/auth");
+          toast.success("Đăng xuất thành công", {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 2000,
+            theme: "dark",
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  };
 
   return (
     <>
